3aefcfaa4f10a9d9aca0fb566e83758f245ee5c4
John Kristian <jmkristian@gmail.com>
2023-03-15 10:20:46 -0700

    Make send.js work with VARA.

--- send.js
+++ send.js
@@ -1,11 +1,10 @@
-/** Send test data to AX.25 clients.
+/** Send test data to clients.
     Send some data each time a line is received from a client.
     If the line starts with a number, send that many frames.
     If there's a second number, send that many bytes per frame.
     By default, send 1 frame of 1024 bytes.
 */
 
-const AGW = require('./agwapi');
 const Config = require('./config').readFile(process.argv[2] || 'config.ini');
 const Readline = require('readline');
 const Stream = require('stream');
@@ -61,7 +60,7 @@ class TestDataStream extends Stream.Readable {
             if (this.r <= this.reps) {
                 var data = newTestData(this.size);
                 data.write(`chunk ${this.r}`, 0, 'ascii');
-                log.info('AX.25 push ' + data.slice(0, 16).toString('binary') + '...');
+                log.info('push ' + data.slice(0, 16).toString('binary') + '...');
                 this.push(data);
             }
         } catch(err) {
@@ -71,33 +70,43 @@ class TestDataStream extends Stream.Readable {
     }
 }
 
-var server = new AGW.Server(Config);
-server.on('error', function(err) {log.warn(err, `AGW error`);});
-server.on('close', function(err) {log.info(`AGW closed`);});
-server.on('connection', function(agw) {
-    log.info('AX.25 connected from %s', agw.theirCall);
-    agw.on('error', function(err) {log.warn(err, `AX.25`);});
-    agw.on('timeout', function(err) {log.warn(err, `AX.25`);});
-    agw.on('finish', function() {log.debug(`AX.25 finished`);});
-    agw.on('end', function() {log.debug(`AX.25 ended`);});
-    agw.on('close', function() {log.info(`AX.25 closed`);});
-    var lines = Readline.createInterface({input: agw});
-    lines.on('line', function(command) {
-        try {
-            if (command == '') {
-                agw.end('Goodbye.\r');
-            } else {
-                var parts = command.split(/\s+/);
-                var reps = parts[0] ? parseInt(parts[0]) : 1;
-                var size = parts[1] ? parseInt(parts[1]) : 1024;
-                new TestDataStream(reps, size).pipe(agw);
+function serve(moduleName, flavor) {
+    const module = require(moduleName);
+    const server = new module.Server(Config, null, flavor);
+    server.on('error', function(err) {log.warn(err, `${moduleName} error`);});
+    server.on('close', function(err) {log.info(`${moduleName} closed`);});
+    server.on('connection', function(connection) {
+        log.info('%s connected from %s', moduleName, connection.theirCall);
+        connection.on('error', function(err) {log.warn(err, `${moduleName}`);});
+        connection.on('timeout', function(err) {log.warn(err, `${moduleName}`);});
+        connection.on('finish', function() {log.debug(`${moduleName} finished`);});
+        connection.on('end', function() {log.debug(`${moduleName} ended`);});
+        connection.on('close', function() {log.info(`${moduleName} closed`);});
+        var lines = Readline.createInterface({input: connection});
+        lines.on('line', function(command) {
+            try {
+                if (command.toLowerCase() == 'b') {
+                    connection.end('Goodbye.\r');
+                } else {
+                    var parts = command.split(/\s+/);
+                    var reps = parts[0] ? parseInt(parts[0]) : 1;
+                    var size = parts[1] ? parseInt(parts[1]) : 1024;
+                    new TestDataStream(reps, size).pipe(connection);
+                }
+            } catch(err) {
+                log.warn(err);
+                connection.end(`${line}: ${err}\r`);
             }
-        } catch(err) {
-            log.warn(err);
-            agw.end(`${line}: ${err}\r`);
-        }
+        });
     });
-});
-server.listen({callTo: Config.AGWPE.myCallSigns}, function(info) {
-    log.info('AGW listening %o', info);
-});
+}
+
+if (Config.AGWPE) {
+    serve('./agwapi');
+}
+if (Config['VARA FM']) {
+    serve('./varaapi', 'FM');
+}
+if (Config['VARA HF']) {
+    serve('./varaapi', 'HF');
+}
